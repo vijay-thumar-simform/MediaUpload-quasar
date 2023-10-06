38fb19d04ff9c198f0992eea0aa6d3f8b1acea9d
vijay-thumar <vijaythumar1020@gmail.com>
2023-10-06 10:49:07 +0530

    completed code for file resume and upload

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,17 +2,29 @@
   <!-- <q-page class="row items-center justify-evenly"> -->
   <q-page class="row items-center justify-evenly">
     <div class="container">
-      <input type="file" id="fileInput" :multiple="'true'" @change="splitFile">
-      <div v-for="(item, index) in files" :key="item.id">
+      <label class="upload-btn">
+        upload
+        <input type="file" id="fileInput" accept="video/*" :multiple="'true'" @change="splitFile" style="display: none;">
+      </label>
+      <!-- <div v-for="(item, index) in files" :key="item.id">
         {{ item.name }}
         <div
           :style="{ minWidth: `${fileUploadPer[index] * 2}px`, maxWidth: `${fileUploadPer[index]}px`, backgroundColor: 'green' }">
           {{ fileUploadPer[index] }}</div>
-        <!-- {{ fileUploadPer[index] }} -->
-        <!-- ! below pause code is working but there are some bugs that we have to solve edge case -->
         <button @click="cancelRequest(item)">Pause</button>
         <button @click="resumeUpload(item)">Resume</button>
+      </div> -->
 
+      <div v-for="file in allFileId" :key="file.file">
+        {{ file.fileName }}
+        <div class="progress-wrapper">
+          <div
+            :style="{ minWidth: `${file.fileUploadPercentage * 2}px`, maxWidth: `${file.fileUploadPercentage * 2}px`, backgroundColor: 'green' }">
+            {{ file.fileUploadPercentage }}%</div>
+        </div>
+        <div v-if="file.fileUploadPercentage >= 100"></div>
+        <button @click="cancelRequest(file)">Pause</button>
+        <button @click="resumeUpload(file)">Resume</button>
       </div>
       <!-- <button @click="cancelRequest">cancel</button> -->
     </div>
@@ -30,6 +42,25 @@
   /* width: 100%; */
   max-width: fit-content;
 }
+
+.upload-btn {
+  background: #607D8B;
+  color: #fff;
+  border-radius: 5px;
+  padding: 15px 20px;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+  font-size: 14px;
+  cursor: pointer;
+}
+
+.progress-wrapper{
+display: inline-block;
+background-color: #bbb;
+min-width: 200px;
+max-width: 200px;
+}
 </style>
 
 <script setup lang="ts">
@@ -43,7 +74,7 @@ const ENDPOINTS = {
 }
 
 const files = ref<any>([])
-const fileUploadPer = ref<any>([])
+// const fileUploadPer = ref<any>([])
 
 let defaultOptions = {
   url: 'http://localhost:1234/upload',
@@ -51,27 +82,28 @@ let defaultOptions = {
   fileId: '',
 };
 
-let allFileId: any = []; // created global file for id, file name,
+const allFileId = ref<any>([]) // created global file for id, file name,
 
 // Chunk upload apiCall
-const uploadFileChunks = (file: any, defaultOptions: any, index: number) => {
+const uploadFileChunks = (file: any, options: any) => {
   // created form data for media to append
   const formData = new FormData();
-  formData.append('file', file, file.name)
-  formData.append('fileId', defaultOptions.fileId)
+  const fileIndex = allFileId.value.findIndex((f: any) => f.fileName === file.name)
+  const selectedFile = allFileId.value[fileIndex]
   // formData.append('fileName', file.name)
-
+  const chunk = file.slice(options.startingByte, file.size);
+  formData.append('file', chunk, file.name)
+  formData.append('fileId', selectedFile.fileId)
   // header for file upload.
-  const chunk = file.slice(defaultOptions.startingByte);
   const headers = {
-    'X-File-Id': defaultOptions.fileId,
-    'Content-Length': chunk.size,
-    'Content-Range': `bytes=${defaultOptions.startingByte}-${defaultOptions.startingByte + chunk.size}/${file.size}`,
+    'X-File-Id': selectedFile.fileId,
+    // 'Content-Length': chunk.size,
+    'Content-Range': `bytes=${options.startingByte}-${options.startingByte + chunk.size}/${file.size}`,
   };
 
   // created token to trace api call request
   const specificToken = axios.CancelToken.source();
-  allFileId.find((f: any) => f.file === file.name && (f.apiToken = specificToken, true)); // add that token globally
+  allFileId.value.find((f: any) => f.fileName === file.name && (f.apiToken = specificToken, true)); // add that token globally
 
   // file upload api call form axios
   axios.post(ENDPOINTS.UPLOAD, formData,
@@ -80,6 +112,12 @@ const uploadFileChunks = (file: any, defaultOptions: any, index: number) => {
       headers: headers,
       onUploadProgress(progressEvent) {
         console.log('process event', progressEvent)
+        if (options.startingByte > 0) {
+          const loaded = options.startingByte + progressEvent.loaded
+          progressEvent.loaded = loaded;
+          const totalSize = selectedFile.file.size
+          progressEvent.total = totalSize
+        }
         let percentage = 0
         if (progressEvent.total) {
           percentage = Math.round(
@@ -87,14 +125,15 @@ const uploadFileChunks = (file: any, defaultOptions: any, index: number) => {
           );
         }
 
-        fileUploadPer.value[index] = percentage
+        // fileUploadPer.value[index] = percentage
+        allFileId.value.find((f: any) => f.fileName === file.name && (f.fileUploadPercentage = percentage, true))
         console.log(`Upload Progress: ${percentage}%`);
       },
       ...defaultOptions
     })
 }
 
-const uploadFile = (file: any, index: number) => {
+const uploadFile = (file: any) => {
   const headers = {
     'Content-Type': 'application/json',
   };
@@ -102,21 +141,22 @@ const uploadFile = (file: any, index: number) => {
   axios.post(ENDPOINTS.UPLOAD_REQUEST, JSON.stringify({ fileName: file.name }), {
     headers: headers
   })
-  .then((res: any) => {
-    console.log('responce form uploadfile request-upload: ', res)
-    allFileId.push({ file: file.name, fileId: res.data.fileId })
-    defaultOptions = { ...defaultOptions, fileId: res.data.fileId }
-    console.log('default options: ', defaultOptions)
-    uploadFileChunks(file, defaultOptions, index) // This is call for file chunk upload
-  })
+    .then((res: any) => {
+      console.log('responce form uploadfile request-upload: ', res)
+      allFileId.value.push({ file: file, fileName: file.name, fileId: res.data.fileId })
+      defaultOptions = { ...defaultOptions, fileId: res.data.fileId }
+      console.log('default options: ', defaultOptions)
+      uploadFileChunks(file, defaultOptions) // This is call for file chunk upload
+    })
 }
 
 const splitFile = (e: any) => {
   console.log('whole event: ', e)
   files.value = Array.from(e.target.files)
   console.log('files that are selected: ', files.value, 'type of files: ', typeof (files.value))
-  files.value.map((file: any, index: number) => {
-    uploadFile(file, index)
+  files.value.map((file: any) => {
+    console.log('file length: ', file.size)
+    uploadFile(file)
   })
   // for (var pair of formData.entries()) {
   //   console.log(pair[0], ',', pair[1])
@@ -125,19 +165,22 @@ const splitFile = (e: any) => {
 }
 
 const resumeUpload = (file: any) => {
-  const specificFile = allFileId.find((f: any) => f.file == file.name)
+  // const specificFile = allFileId.value.find((f: any) => f.fileName == file.name)
 
-  const query = `${ENDPOINTS.UPLOAD_STATUS}?fileName=${file.name}&fileId=${specificFile.fileId}`
+  const query = `${ENDPOINTS.UPLOAD_STATUS}?fileName=${file.fileName}&fileId=${file.fileId}`
 
   axios.get(query)
-    .then((res) => {
-      console.log(res)
+    .then((res: any) => {
+      console.log('defaultOptions --> res: ', res)
+      defaultOptions.startingByte = res.data.totalChunkUploaded
+      uploadFileChunks(file.file, { ...defaultOptions, startingByte: res.data.totalChunkUploaded })
     }).catch((e) => { console.log('error while calling an resume: ', e) })
 }
 
 const cancelRequest = (file: any) => {
-  allFileId.find((f: any) => {
-    f.file == file.name && f.apiToken.cancel('Request canceled by user.'), true
-  })
+  // allFileId.value.find((f: any) => {
+  //   f.fileName == file.name && f.apiToken.cancel('Request canceled by user.'), true
+  // })
+  file.apiToken.cancel('Request canceled by user.')
 };
 </script>
